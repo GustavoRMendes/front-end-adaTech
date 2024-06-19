5afe1658f2f9508a874a54d861312e313ee33d88
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-19 11:10:17 -0400

    Add variables and data types

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,11 @@
 console.log("Hello, World!");
 console.log("Estamos na nossa aula 06!");
+
+// Variáveis (const, var, let)
+const texto = "Mensagem"; // String
+const numero = 10; // Number
+const estudando = true; // Boolean
+
+console.log(typeof texto);
+console.log(typeof numero);
+console.log(typeof estudando);
