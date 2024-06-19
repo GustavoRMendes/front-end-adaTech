9e27dcf1aabe19243116284c0422d81e4becad42
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-19 11:14:32 -0400

    add grade averages

--- javascript/main.js
+++ javascript/main.js
@@ -2,10 +2,19 @@ console.log("Hello, World!");
 console.log("Estamos na nossa aula 06!");
 
 // Variáveis (const, var, let)
+// ! Não utilize o var no seus códigos.
 const texto = "Mensagem"; // String
 const numero = 10; // Number
 const estudando = true; // Boolean
+const indefinido = undefined; // Undefined
 
 console.log(typeof texto);
 console.log(typeof numero);
 console.log(typeof estudando);
+console.log(typeof indefinido);
+
+// Operadores matemáticos
+const nota1 = 10;
+const nota2 = 8;
+const media = (nota1 + nota2) / 2;
+console.log("A média das notas resulta em: " + media);
