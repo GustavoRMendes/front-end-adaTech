f8bcd6c8521c0a645df5e74a057ec9e761ba4a71
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-19 22:11:25 -0400

    Add more examples

--- javascript/condicional.js
+++ javascript/condicional.js
@@ -20,7 +20,19 @@ if (true) {
 } else {
   console.log("Condição 3");
 }
+const nota1 = 70;
+const nota2 = 80;
+const media = (nota1 + nota2) / 2;
 
+if (media >= 90) {
+  console.log("Aluno aprovado com nota A!");
+} else if (media >= 80) {
+  console.log("Aluno aprovado com nota B!");
+} else if (media >= 70) {
+  console.log("Aluno aprovado com nota C!");
+} else {
+  console.log("Aluno reprovado!");
+}
 // ternário
 const idade = 19;
 const verificacao = idade > 18 ? "Maior" : "Menor";
