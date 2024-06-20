3186a61fe7042e0f139ba85563b6b35c715b99e4
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-19 22:05:39 -0400

    Add conditional if else

--- javascript/condicional.js
+++ javascript/condicional.js
@@ -1,4 +1,11 @@
 // if
 if (true) {
-  console.log("Condição verdadeira");
+  console.log("Condição");
+}
+
+// if else
+if (false) {
+  console.log("Condição");
+} else {
+  console.log("Condição 2");
 }
