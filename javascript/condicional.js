69080776b8218eeb39f97c7c328556a80156d5dd
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-21 21:28:42 -0400

    Add operators boolean

--- javascript/condicional.js
+++ javascript/condicional.js
@@ -1,5 +1,12 @@
 // Condicionais
 
+// Operadores Booleanos: Comparações
+const numero1 = 10;
+const numero2 = 12;
+console.log(numero1 === numero2);
+console.log(numero1 == numero2);
+console.log(numero1 == "10");
+
 // if
 if (true) {
   console.log("Condição");
