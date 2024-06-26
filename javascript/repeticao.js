29c5842d1933a5108363deae49715e91865f41e1
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-26 10:09:33 -0400

    Add game using do while

--- javascript/repeticao.js
+++ javascript/repeticao.js
@@ -1,53 +1,68 @@
-// Estruturas de Repetição
+// // Estruturas de Repetição
 
-// While
-let saldo = Number(prompt("Qual o valor do seu saldo? "));
+// // While
+// let saldo = Number(prompt("Qual o valor do seu saldo? "));
 
-while (saldo < 0) {
-  saldo = Number(prompt("Seu saldo não pode ser negativo. Tente novamente:"));
-}
-alert("Seu saldo é " + saldo);
+// while (saldo < 0) {
+//   saldo = Number(prompt("Seu saldo não pode ser negativo. Tente novamente:"));
+// }
+// alert("Seu saldo é " + saldo);
 
-// Do While
-let numero = 10;
+// // Do While
+// let numero = 10;
 
+// do {
+//   numero += 1;
+//   alert(numero);
+// } while (numero < 15);
+
+// do {
+//   let opcao = Number(
+//     prompt(`
+//     Escolha uma das opções inserindo o número de acordo com a opção desejada.
+//     1 - Mostrar meu nome.
+//     2 - Mostrar meu sobrenome.
+//     3 - Mostrar minha idade.
+//     4 - Mostrar minha profissão.
+//     5 - Sair do programa.
+//   `)
+//   );
+//   switch (opcao) {
+//     case 1:
+//       const nome = prompt("Qual seu nome? ");
+//       alert("Seu nome é " + nome);
+//       break;
+//     case 2:
+//       const sobrenome = prompt("Qual seu sobrenome?");
+//       alert("Seu sobrenome é " + sobrenome);
+//       break;
+//     case 3:
+//       const idade = Number(prompt("Qual a sua idade?"));
+//       alert("Sua idade é " + idade);
+//       break;
+//     case 4:
+//       const profissao = prompt("Qual sua profissão?");
+//       alert("Sua profissão é " + profissao);
+//       break;
+//     case 5:
+//       alert("Saindo...");
+//       break;
+//     default:
+//       alert("ERRO. Insira um número corretamente");
+//   }
+// } while (opcao !== 5);
+
+let numeroAleatorio = Math.floor(Math.random() * 101);
+let chute;
+let tentativas = 0;
 do {
-  numero += 1;
-  alert(numero);
-} while (numero < 15);
-
-do {
-  let opcao = Number(
-    prompt(`
-    Escolha uma das opções inserindo o número de acordo com a opção desejada.
-    1 - Mostrar meu nome.
-    2 - Mostrar meu sobrenome.
-    3 - Mostrar minha idade.
-    4 - Mostrar minha profissão.
-    5 - Sair do programa.
-  `)
-  );
-  switch (opcao) {
-    case 1:
-      const nome = prompt("Qual seu nome? ");
-      alert("Seu nome é " + nome);
-      break;
-    case 2:
-      const sobrenome = prompt("Qual seu sobrenome?");
-      alert("Seu sobrenome é " + sobrenome);
-      break;
-    case 3:
-      const idade = Number(prompt("Qual a sua idade?"));
-      alert("Sua idade é " + idade);
-      break;
-    case 4:
-      const profissao = prompt("Qual sua profissão?");
-      alert("Sua profissão é " + profissao);
-      break;
-    case 5:
-      alert("Saindo...");
-      break;
-    default:
-      alert("ERRO. Insira um número corretamente");
+  chute = Number(prompt("Chute um número: "));
+  tentativas += 1;
+  if (chute < numeroAleatorio) {
+    alert("Tente um número maior.");
+  } else if (chute > numeroAleatorio) {
+    alert("Tente um número menor.");
+  } else {
+    alert("Parabéns! Você acertou em " + tentativas + " tentativas.");
   }
-} while (opcao !== 5);
+} while (chute !== numeroAleatorio);
