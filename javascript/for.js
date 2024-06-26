20dda68c9b2582b31abd1406d7e7dbd6a78febde
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-26 10:45:46 -0400

    Add arrays and slice

--- javascript/for.js
+++ javascript/for.js
@@ -3,3 +3,12 @@ let cont;
 for (cont = 0; cont <= 10; cont++) {
   console.log(cont);
 }
+// * Arrays
+const notas = [8, 10, 7];
+notas[2] = 9;
+for (cont = 0; cont < notas.length; cont++) {
+  console.log(notas[cont]);
+}
+
+const numeros = [1, 2, 3, 4, 5, 6, 7];
+console.log(numeros.slice(0, 4));
