29a43d1012de65ee0ef4b85ff3b4289291e996f7
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-26 10:50:18 -0400

    Add includes, indexOf and lastIndexOf

--- javascript/for.js
+++ javascript/for.js
@@ -10,7 +10,7 @@ for (cont = 0; cont < notas.length; cont++) {
   console.log(notas[cont]);
 }
 
-const numeros = [1, 2, 3, 4, 5, 6, 7];
+const numeros = [1, 2, 3, 4, 5, 6, 7, 5];
 console.log(numeros.slice(0, 4));
 console.log(numeros.push(8));
 console.log(numeros.unshift(0));
@@ -19,3 +19,6 @@ console.log(numeros);
 numeros.pop();
 numeros.shift();
 console.log(numeros);
+console.log(numeros.includes(8)); // false
+console.log(numeros.indexOf(3)); // 2
+console.log(numeros.lastIndexOf(5)); // 7
