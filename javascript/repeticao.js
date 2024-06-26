4845b2603335229ce38e869efa84ba25604023ad
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-26 09:44:09 -0400

    Descomment code

--- javascript/repeticao.js
+++ javascript/repeticao.js
@@ -1,20 +1,20 @@
 // Estruturas de Repetição
 
-// // While
-// let saldo = Number(prompt("Qual o valor do seu saldo? "));
+// While
+let saldo = Number(prompt("Qual o valor do seu saldo? "));
 
-// while (saldo < 0) {
-//   saldo = Number(prompt("Seu saldo não pode ser negativo. Tente novamente:"));
-// }
-// alert("Seu saldo é " + saldo);
+while (saldo < 0) {
+  saldo = Number(prompt("Seu saldo não pode ser negativo. Tente novamente:"));
+}
+alert("Seu saldo é " + saldo);
 
-// // Do While
-// let numero = 10;
+// Do While
+let numero = 10;
 
-// do {
-//   numero += 1;
-//   alert(numero);
-// } while (numero < 15);
+do {
+  numero += 1;
+  alert(numero);
+} while (numero < 15);
 
 do {
   let opcao = Number(
