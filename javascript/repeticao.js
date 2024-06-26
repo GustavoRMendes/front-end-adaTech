3bf55b5f0ea75e3b36792451cebea7d019760be8
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-26 09:31:28 -0400

    Add example for while

--- javascript/repeticao.js
+++ javascript/repeticao.js
@@ -1,5 +1,10 @@
 // Estruturas de Repetição
 
 // While
+let saldo = Number(prompt("Qual o valor do seu saldo? "));
 
+while (saldo < 0) {
+  saldo = Number(prompt("Seu saldo não pode ser negativo. Tente novamente:"));
+}
+alert("Seu saldo é " + saldo);
 // Do While
