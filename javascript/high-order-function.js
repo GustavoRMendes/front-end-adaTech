8b69cebd833380a1332728d8725afc36a471a286
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-26 21:27:23 -0400

    function(function)

--- javascript/high-order-function.js
+++ javascript/high-order-function.js
@@ -3,3 +3,10 @@
 function retornoFuncao() {
   return () => console.log("Retorno");
 }
+
+const somar = (numero1, numero2) => numero1 + numero2;
+
+function calcular(funcaoDeSoma) {
+  return console.log(funcaoDeSoma(20, 20));
+}
+calcular(somar);
