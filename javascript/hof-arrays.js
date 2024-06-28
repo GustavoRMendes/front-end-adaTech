7199f381e424b1a340c1b89e3d37047a63e007d2
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-27 22:47:34 -0400

    Array.reduce()

--- javascript/hof-arrays.js
+++ javascript/hof-arrays.js
@@ -36,3 +36,15 @@ const resultado = listaDeNumeros.filter((numero) => {
   return numero > 80;
 });
 console.log(resultado);
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
