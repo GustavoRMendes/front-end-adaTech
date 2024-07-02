9b110987865d01c93f32789eb1c5fbadba3ce1ed
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-02 09:38:08 -0400

    Create new promise

--- assincrono/assincronismo.js
+++ assincrono/assincronismo.js
@@ -12,8 +12,30 @@ const fs = require("fs");
 // console.log("Depois da função de ler o arquivo");
 
 // setTimeout()
-console.log("Antes do setTimeout");
-setTimeout(() => {
-  console.log("Esse console.log vai ser executado após 2 segundos.");
-}, 2000);
-console.log("Depois do setTimeout");
+// console.log("Antes do setTimeout");
+// setTimeout(() => {
+//   console.log("Esse console.log vai ser executado após 2 segundos.");
+// }, 2000);
+// console.log("Depois do setTimeout");
+
+// Promises
+const promessa = new Promise((resolve, reject) => {
+  fs.readFile("assincrono/arquivo.txt", (erro, conteudo) => {
+    if (erro) {
+      reject("Ocorreu um erro: " + erro);
+    } else {
+      resolve(String(conteudo));
+    }
+  });
+});
+
+promessa
+  .then((conteudoDoRetornoDaPromise) => {
+    console.log("Deu certo: \n" + conteudoDoRetornoDaPromise);
+  })
+  .catch((erro) => {
+    console.log("Deu ruim: \n" + erro);
+  })
+  .finally(() => {
+    console.log("Promessa encerrada.");
+  });
