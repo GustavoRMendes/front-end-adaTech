dc703b73599dbb60b17954fc03f798e338eb498d
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-02 10:42:12 -0400

    JSON.parse()

--- javascript-json/index.js
+++ javascript-json/index.js
@@ -1,2 +1,12 @@
 const aula = 'JSON';
 console.log(aula);
+const fs = require('fs');
+// Ler arquivo JSON
+fs.readFile('javascript-json/.prettierrc.json', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(data));
+    console.log(typeof data);
+  }
+});
