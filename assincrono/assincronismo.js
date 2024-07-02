7c4ebeb54c3f4fd7c555827787a9b8c0d9dcfd6e
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-02 09:23:10 -0400

    setTimeout()

--- assincrono/assincronismo.js
+++ assincrono/assincronismo.js
@@ -1,12 +1,19 @@
 const fs = require("fs");
 
 // * Callback
-console.log("Antes da função de ler o arquivo");
-fs.readFile("assincrono/arquivo.txt", (erro, conteudo) => {
-  if (erro) {
-    console.log("Ocorreu um erro");
-  } else {
-    console.log(String(conteudo));
-  }
-});
-console.log("Depois da função de ler o arquivo");
+// console.log("Antes da função de ler o arquivo");
+// fs.readFile("assincrono/arquivo.txt", (erro, conteudo) => {
+//   if (erro) {
+//     console.log("Ocorreu um erro");
+//   } else {
+//     console.log(String(conteudo));
+//   }
+// });
+// console.log("Depois da função de ler o arquivo");
+
+// setTimeout()
+console.log("Antes do setTimeout");
+setTimeout(() => {
+  console.log("Esse console.log vai ser executado após 2 segundos.");
+}, 2000);
+console.log("Depois do setTimeout");
