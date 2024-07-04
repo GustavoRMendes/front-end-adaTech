b09378d8178690e5ef3b11c3a9fb46847fe96861
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-04 09:39:27 -0400

    Comment fetch with then and catch

--- fetch-api/js/index.js
+++ fetch-api/js/index.js
@@ -1,12 +1,12 @@
 console.log("Fetch API");
 
-fetch("http://viacep.com.br/ws/76824436/json/")
-  .then((resposta) => {
-    resposta.json().then((dados) => console.log(dados));
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// fetch("http://viacep.com.br/ws/76824436/json/")
+//   .then((resposta) => {
+//     resposta.json().then((dados) => console.log(dados));
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 async function obterDados() {
   try {
