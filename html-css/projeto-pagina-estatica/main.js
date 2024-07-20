0547c602ea144eca969d74ead49c2075f7d2efd4
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-19 23:50:11 -0400

    adicionar evento no brand

--- html-css/projeto-pagina-estatica/main.js
+++ html-css/projeto-pagina-estatica/main.js
@@ -1,5 +1,6 @@
 const bannerContentBtn = document.querySelector(".banner-content-btn");
 const links = document.querySelectorAll(".nav-item");
+const brand = document.querySelector(".nav-brand");
 bannerContentBtn.addEventListener("click", () => {
   const confirmacao = confirm("Tem certeza que quer baixar?");
   if (confirmacao) {
@@ -13,4 +14,8 @@ links.forEach((link) => {
   link.addEventListener("click", () => {
     alert("Clique no link");
   });
+});
+
+brand.addEventListener("click", () => {
+  alert("Bem-vindo ao Portfólio do Gustavo!");
 });
