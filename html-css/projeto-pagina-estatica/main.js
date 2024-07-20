3fabb3a36f59d68279e87ab53d71ee6859157b9c
Gustavo Mendes <gustavormendess@gmail.com>
2024-07-19 23:45:39 -0400

    adicionar confirmação no evento

--- html-css/projeto-pagina-estatica/main.js
+++ html-css/projeto-pagina-estatica/main.js
@@ -1,4 +1,9 @@
 const bannerContentBtn = document.querySelector(".banner-content-btn");
 bannerContentBtn.addEventListener("click", () => {
-  alert("Arquivo baixado.");
+  const confirmacao = confirm("Tem certeza que quer baixar?");
+  if (confirmacao) {
+    alert("Arquivo baixado.");
+  } else {
+    alert("Download cancelado.");
+  }
 });
